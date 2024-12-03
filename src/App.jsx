75f94d3170f10e64965fa764610a3fb2a640a3c5
OC <chandankarom07@gmail.com>
2024-12-03 16:37:50 +0530

    Updated code in 2 files and use the commented code in the hreo section to turn the cards back on

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HeroSection from "./sections/HeroSection.jsx";
 
 function App() {
     return (
-        <main className="relative bg-black font-jetbrains overflow-hidden" >
+        <main className=" font-jetbrains " >
             <div className="lines">
                 <div className="line -z-2"></div>
                 <div className="line -z-2" ></div>
@@ -12,6 +12,9 @@ function App() {
             </div>
             <Navbar/>
             <div className="padding">
+                <HeroSection/>
+            </div>
+            <div>
                 <HeroSection/>
             </div>
         </main>
